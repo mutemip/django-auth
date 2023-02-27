e63312d38da206de72128b972ea3a40b4407f390
mutemip <paulmutemi2030@gmail.com>
2023-02-28 01:45:58 +0300

    view: added register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth import login
+from django.contrib import messages
 
-# Create your views here.
+
+
+# Our register view
+def Register_View(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save() # save passed data
+            login(request, user) # login the user
+            messages.success(request, "Registration Successful")
+            return redirect("/")
+        messages.error(request, "Invalid information")
+    return render(request, "register.html", {"register_form": form})
+
